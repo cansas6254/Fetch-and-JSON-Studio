f48c6076b6cbc789291779f47f392a466bf76a66
Cansas Bell <cansas6254@gmail.com>
2022-03-10 20:08:51 -0600

    added some code

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+        .then(function (response) {
+            response.json()
+                .then(function (json) {
+                    console.log(json);
+                    const div = document.getElementById("container");
+                    // Add HTML that includes the JSON data
+                    for (let i=0; i<json.length; i++){
+                        div.innerHTML += `
+                        <div class="astronaut">
+                        <div class="bio">
+                           <h3>Mae Jemison</h3>
+                           <ul>
+                              <li>Hours in space: 190</li>
+                              <li>Active: false</li>
+                              <li>Skills: Physician, Chemical Engineer</li>
+                           </ul>
+                        </div>
+                        <img class="avatar" src="images/mae-jemison.jpg">
+                     </div>
+                        `; 
+                    }
+                   
+                });
+        });
+});
