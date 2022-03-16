d3149f690823549495f635ce580ab6d8f262afff
Cansas Bell <cansas6254@gmail.com>
2022-03-16 14:09:35 -0500

    added main

--- script.js
+++ script.js
@@ -2,27 +2,33 @@
 window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json")
         .then(function (response) {
-            response.json()
-                .then(function (json) {
-                    console.log(json);
-                    const div = document.getElementById("container");
-                    // Add HTML that includes the JSON data
-                    for (let i=0; i<json.length; i++){
-                        div.innerHTML += `
-                        <div class="astronaut">
-                        <div class="bio">
-                           <h3>Mae Jemison</h3>
-                           <ul>
-                              <li>Hours in space: 190</li>
-                              <li>Active: false</li>
-                              <li>Skills: Physician, Chemical Engineer</li>
-                           </ul>
-                        </div>
-                        <img class="avatar" src="images/mae-jemison.jpg">
-                     </div>
-                        `; 
-                    }
+            return response.json();
+        }).then(function (json) {
+
+
+
+            let sortedAstronauts = response.sort(function (a, b) { return a.hoursInSpace })
+            //console.log(json);
+
+
+            let myContainer = document.getElementById('container');
+
+            let allMyHtml = "";
+            sortedAstronauts.forEach(function (astronaut) {
+                let allMyHtml = `
+                    < div class="astronaut" >
+                            <div class="bio">`
+                    < h3 > ${ astronaut, firstName } ${ astronaut, lastName }</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace</li>
+                            <li>${astronaut.active ? "class="green-text" ' : ''}>Active: ${astronaut.active}</li>/li>
+                            <li>Skills: Physician, Chemical Engineer</li>
+                        </ul>
+                        </div >
+                    <img class="avatar" src="images/mae-jemison.jpg">
+                    </div>
+                    ;
+            }
                    
                 });
-        });
 });
